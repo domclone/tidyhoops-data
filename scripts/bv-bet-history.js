2fa779f241d01a09fa1c3dac2f1bc1643d858dfa
dom d'onofrio <domenic2692@gmail.com>
2021-01-17 15:36:34 -0500

    getPlayerHeadshotUrl function

--- scripts/bv-bet-history.js
+++ scripts/bv-bet-history.js
@@ -1,10 +1,10 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const Bet = require('../models/bet');
-const importJsonFromRestApi = require('../toolkits/extract/importJsonFromRestApi.js')
+const importJsonFromRestApi = require('../toolkits/extract/importJsonFromRestApi.js');
 const getRequestUrl = require('../toolkits/extract/getRequestUrl.js');
+const getPlayerHeadshotUrl = require('../toolkits/extract/getPlayerHeadshotUrl.js');
 const { connect } = require('../mongoose');
-const nba = require('nba-api-client');
 
 const bvUrl = 'https://www.bovada.lv/sports/player-props?overlay=login'
 const bvBetHistoryRoute = '**/api/mybets*'
@@ -21,10 +21,8 @@ const betHistory = async () => {
   try {
     // get request url for bet data from bovada api
     let requestUrl = await getRequestUrl(bvUrl, bvBetHistoryRoute, credentials, selectors);
-
     requestUrl.searchParams.set('bet.isActive', '0'); // we want settled bets not active bets
     requestUrl.searchParams.delete('limit'); // we want all bets 
-
     requestUrl = requestUrl.href
 
     const rawBets = await importJsonFromRestApi(requestUrl);
@@ -32,7 +30,6 @@ const betHistory = async () => {
     const cleanedBets = rawBets.data.map(entry => {
       let playerName = entry.events[0].players !== null ? entry.events[0].players[0].name : null
       if (playerName === 'C.J. McCollum') playerName = 'CJ McCollum' // annoying, will find a better solution
-      const nbaIds = nba.getPlayerID(playerName) ? nba.getPlayerID(playerName) : null // uses nba-api-client package
 
       const bet = {
         id: entry.id,
@@ -48,10 +45,9 @@ const betHistory = async () => {
         result: entry.events[0].settlement.result,
         gain: entry.events[0].settlement.result === 'Lost' ? -Math.round(entry.amount) : Math.round((entry.odds * entry.amount) - entry.amount),
         isParlay: entry.isAccumulator,
-        nbaPlayerId: nbaIds === null ? null : nbaIds.PlayerID,
-        nbaTeamId: nbaIds === null ? null : nbaIds.TeamID,
-        playerHeadshot: nbaIds === null ? null : `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${nbaIds.PlayerID}.png`
+        playerHeadshot: getPlayerHeadshotUrl(playerName)
       }
+
       return bet;
     })
     const straightBets = cleanedBets.filter(bet => bet.isParlay === 0); // we want to filter out parlays for now
